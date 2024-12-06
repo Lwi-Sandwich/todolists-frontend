b6cab4f2c5ace8bca6d4bfa3d40625cb8a3c210b
Louis Bruneteau <bruneteau.louis@gmail.com>
2024-12-06 12:01:14 +0100

    ça faire le marchage

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,55 +1,39 @@
 // import { ITodoList, TodoListStatus } from './api-types'
-import { ListsApi } from 'todo-list-client'
+import { ItemsApi, ListsApi } from 'todo-list-client'
 import axios from 'axios'
 
-const lists = [
+const api = new ListsApi(
     {
-        id: '1',
-        name: 'Work Tasks'
-    }, 
-    {
-        'id': '2',
-        name: 'Personal Tasks'
-    }, 
-    {
-        'id': '3',
-        name: 'Shopping List'
-    }
-]
-const listItems: Record<string, string[]> = {
-    'Work Tasks': ['Buy groceries', 'Complete React project', 'Exercise for 30 minutes', 'Read a book chapter'],
-    'Personal Tasks': ['Buy groceries', 'Complete React project', 'Exercise for 30 minutes', 'Read a book chapter'],
-    'Shopping List': ['Buy groceries', 'Complete React project', 'Exercise for 30 minutes', 'Read a book chapter']
-}
+        isJsonMime: (mime: string) => mime.startsWith('application/json')
+    },
+    'http://localhost:3000',
+    axios,
+)
 
-// const api = new ListsApi(
-//     {
-//         isJsonMime: (mime: string) => mime.startsWith('application/json')
-//     },
-//     'http://localhost:3000',
-//     axios,
-// )
+const apiItem = new ItemsApi(
+    {
+        isJsonMime: (mime: string) => mime.startsWith('application/json')
+    },
+    'http://localhost:3000',
+    axios,
+)
 
 export const apiClient = {
     getLists: async () => {
-        return Promise.resolve(lists)
-        // const response = await api.listsGet()
-        // return response.data
+        const response = await api.listsGet()
+        return response.data
     },
     addList: async (listName: string) => {
-        lists.push({id: listName, name: listName})
-        console.debug('-- addList', listName, lists);
-        return Promise.resolve(lists)
+        await api.listsPost({name: listName})
+        return apiClient.getLists()
     },
     getTodos: async (listName: string): Promise<string[]> => {
-        return Promise.resolve(listItems[listName])
+        const response = await apiItem.listsListIdItemsGet(listName);
+        const items = response.data;
+        return items.map((item) => item.name);
     },
     addTodo: async (listName: string, todo: string) => {
-        console.debug('-- addTodo', listName, todo, listItems);
-        if (!listItems[listName]) {
-            listItems[listName] = []
-        }
-        listItems[listName].push(todo)
-        return Promise.resolve(listItems[listName])
+        await apiItem.listsListIdItemsPost(listName, {name: todo, status: 'PENDING'})
+        return apiClient.getTodos(listName)
     }
 }
